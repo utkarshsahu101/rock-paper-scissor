e768a3869b93fc1c8e8fe6e7cd993336e4a93d3b
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-07-09 13:14:05 +0530

    rock, paper, scissor game

---
+++ README.md
@@ -0,0 +1,7 @@
+# DESCRIPTION
+
+This is a Rock Paper Scissor game in which user an play with computer.
+
+> User can win or lose by clicking on any of the three icons
+
+> Score will get updated on score-board
---
+++ app.js
@@ -0,0 +1,83 @@
+// dom variables
+const userScore = document.getElementById("userScore");
+const compScore = document.getElementById("compScore");
+const scoreBoard = document.querySelector(".scoreBoard");
+const result = document.querySelector(".result > p");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+
+// js variables
+let user = 0;
+let comp = 0;
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissor"];
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
+}
+// console.log(getComputerChoice());
+
+function win(userChoice, computerChoice) {
+  const smallUser = "user".fontsize(3).sub();
+  const smallComp = "comp".fontsize(3).sub();
+  const userChoiceDiv = document.getElementById(userChoice);
+  user++;
+  userScore.innerHTML = user;
+  compScore.innerHTML = comp;
+
+  result.innerHTML = `${userChoice}${smallUser} beats ${computerChoice}${smallComp}. You win....`;
+  userChoiceDiv.classList.add("green-shadow");
+  setTimeout(() => userChoiceDiv.classList.remove("green-shadow"), 300);
+}
+function lose(userChoice, computerChoice) {
+  const smallUser = "user".fontsize(3).sub();
+  const smallComp = "comp".fontsize(3).sub();
+  const userChoiceDiv = document.getElementById(userChoice);
+  comp++;
+  userScore.innerHTML = user;
+  compScore.innerHTML = comp;
+
+  result.innerHTML = `${userChoice}${smallUser} loses to  ${computerChoice}${smallComp}. You lost....`;
+  userChoiceDiv.classList.add("red-shadow");
+  setTimeout(() => userChoiceDiv.classList.remove("red-shadow"), 300);
+}
+function draw(userChoice, computerChoice) {
+  const smallUser = "user".fontsize(3).sub();
+  const smallComp = "comp".fontsize(3).sub();
+  const userChoiceDiv = document.getElementById(userChoice);
+  userScore.innerHTML = user;
+  compScore.innerHTML = comp;
+
+  result.innerHTML = `${userChoice}${smallUser} equals ${computerChoice}${smallComp}. It's a draw.`;
+  userChoiceDiv.classList.add("black-shadow");
+  setTimeout(() => userChoiceDiv.classList.remove("black-shadow"), 300);
+}
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rockscissor":
+    case "scissorpaper":
+    case "paperrock":
+      win(userChoice, computerChoice);
+      break;
+    case "scissorrock":
+    case "paperscissor":
+    case "rockpaper":
+      lose(userChoice, computerChoice);
+      break;
+    case "scissorscissor":
+    case "paperpaper":
+    case "rockrock":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+function main() {
+  rock.addEventListener("click", () => game("rock"));
+  paper.addEventListener("click", () => game("paper"));
+  scissor.addEventListener("click", () => game("scissor"));
+}
+
+main();
